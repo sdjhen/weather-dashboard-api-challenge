cf781ede5669a10ac0fe4ddb76bc82a873651f69
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-01-31 16:42:08 +0000

    5 day forecast API call success

--- script.js
+++ script.js
@@ -14,5 +14,23 @@ $('#search-form').on('submit', function (e) {
   // Geolocation API Call
   $.ajax({ url: queryURL }).then(function (response) {
     console.log(response);
+
+    // Get lat and long for forecast API
+    const lat = response[0].lat;
+    const lon = response[0].lon;
+
+    console.log(lat, lon);
+
+    // Weather forecast API Call
+    wForecastQueryURL =
+      'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' +
+      lat +
+      '&lon=' +
+      lon +
+      '&appid=' +
+      apiKey;
+    $.ajax({ url: wForecastQueryURL }).then(function (forecastResponse) {
+      console.log(forecastResponse);
+    });
   });
 });
